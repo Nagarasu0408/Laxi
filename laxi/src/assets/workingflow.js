cdffb649837adbcd0cef986f98034985e53adab5
NAVEENKUMAR K <86141832+knk-naviin@users.noreply.github.com>
2024-06-09 15:34:56 +0530

    workingflow

--- laxi/src/assets/workingflow.js
+++ laxi/src/assets/workingflow.js
@@ -3,47 +3,61 @@ import selection from './Images/TechnologyStack/selection1.jpg';
 import developing from './Images/TechnologyStack/development.png';
 import testing from './Images/TechnologyStack/testing1.jpg';
 function WorkingFlow() {
-      return (
-      <div className="container">
-        <div className="row">
-            <div className="col-5">
-               <img src={planning} alt='planning' className='Images11'></img>
+   return (
+   <div className='container'>
+      <div className="section-container-1">
+           <div className='column-image'>
+            <img src={planning} alt='planning'></img>            
+           </div>
+           <div className='column-content'>
+            <div className='context-container'>
+               <h3>Planning and Services</h3>
+               <p>Using cloud services for mobile application development involves leveraging various cloud platforms to handle backend services, 
+                  storage, databases, authentication,  and more. Here’s a comprehensive guide to help you get started</p>
             </div>
-            <div className="col-7" id="planning">
-               <h1>Planning and Service</h1>
-               <p>Defining the app’s goals, target audience, features, and cloud integration needs. The process includes designing wireframes, setting timelines, and budgeting. Development involves building the app’s front-end and back-end, leveraging cloud services for storage, authentication, and real-time data synchronization. Ensuring robust security and compliance with cloud providers is crucial.</p>
-            </div>
-        </div>
-        <div className="row">
-        <div className="col-7" id='selecting' >
-        <h1>Selecting Cloud Services</h1>
-        <p>Selecting cloud services for mobile app development involves <br></br>evaluating providers based on your app's requirements. Key considerations<br></br> include scalability, ease of integration, cost, security, and available features. <br></br>Popular options are Firebase (Google) for real-time databases and seamless authentication, AWS (Amazon) for extensive infrastructure and machine learning capabilities, and Microsoft Azure for robust enterprise solutions and AI services. </p>
-        </div>
-       <div className="col-5">
-       <img src={selection} alt='selection' className='Images11'></img>
-       </div>
-       </div>
-       <div className="row">
-    <div className="col-5">
-       <img src={developing} alt='planning' className='Images11'></img>
-    </div>
-    <div className="col-7" id='developing'>
-       <h1>Development</h1>
-       <p>First, choose a cloud provider like Firebase, AWS, or Azure. Begin with defining the app’s features and user requirements. Develop the front-end using frameworks like React Native or Flutter. Integrate cloud services for authentication, database management, storage, and real-time data syncing. Implement security protocols to protect data. Perform rigorous testing to ensure functionality and performance. Deploy the app on platforms like Google Play and Apple App Store.</p>
-    </div>
-</div>
-<div className="row">
- <div className="col-7" id='testing'>
- <h1>Testing</h1>
- <p>Testing mobile apps integrated with cloud services involves several critical steps. First, functional testing ensures that all features work as intended, including cloud-based authentication and data synchronization. Performance testing evaluates the app’s responsiveness and load-handling capabilities, particularly during peak usage times. Security testing is crucial to protect user data and prevent breaches.</p>
- </div>
-<div className="col-5">
-<img src={testing} alt='planning' className='Images11'></img>
-</div>
-</div>
-      </div> 
-    );
-   
-}
+           </div>
+      </div>
 
+      <div className="section-container-2">
+     <div className='column-content'>
+      <div className='context-container'>
+         <h3>Planning and Services</h3>
+         <p>Using cloud services for mobile application development involves leveraging various cloud platforms to handle backend services, 
+            storage, databases, authentication,  and more. Here’s a comprehensive guide to help you get started</p>
+      </div>
+     </div>
+     <div className='column-image2'>
+      <img src={selection} alt='planning'></img>            
+     </div>
+     </div>
+
+<div className="section-container-3">
+     <div className='column-image'>
+      <img src={developing} alt='planning'></img>            
+     </div>
+     <div className='column-content'>
+      <div className='context-container'>
+         <h3>Planning and Services</h3>
+         <p>Using cloud services for mobile application development involves leveraging various cloud platforms to handle backend services, 
+            storage, databases, authentication,  and more. Here’s a comprehensive guide to help you get started</p>
+      </div>
+     </div>
+</div>
+<div className="section-container-4">
+  <div className='column-content'>
+   <div className='context-container'>
+      <h3>Planning and Services</h3>
+      <p>Using cloud services for mobile application development involves leveraging various cloud platforms to handle backend services, 
+         storage, databases, authentication,  and more. Here’s a comprehensive guide to help you get started</p>
+   </div>
+  </div>
+  <div className='column-image2'>
+   <img src={testing} alt='planning'></img>            
+  </div>
+  </div>
+   </div>
+
+   
+   );
+}
 export default WorkingFlow;
